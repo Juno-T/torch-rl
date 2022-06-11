ec74b58219ada3da2f6e6ab97870987b91d1e361
Juno-T <yossathorn.tian@gmail.com>
2022-06-11 12:48:10 +0900

    init base, random agents

---
+++ agents/base.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+import time
+
+import numpy as np
+
+class Agent(ABC):
+  def __init__(self):
+    pass
+
+  @abstractmethod
+  def train_init(self):
+    pass
+
+
+  @abstractmethod
+  def episode_init(self, initial_observation):
+    pass
+
+  @abstractmethod
+  def act(self):
+    pass
+
+  def eval_act(self):
+    return self.act(observation)
+
+  def observe(self, observation):
+    raise("Not implemented")
+  
+  def remember(self, action, timestep):
+    raise("Not implemented")
+
+  def get_stats(self):
+    raise("Not implemented")
+
+  def learn_one_ep(self, episode):
+    raise("Not implemented")
+
+  def learn_batch_transitions(self, transitions):
+    raise("Not implemented")
+
+class RandomAgent(Agent):
+  def __init__(self, env, memory=None, learning_rate=0.1):
+    self.env = env
+    self.memory = memory
+    self.state_space = env.observation_space
+    self.action_space = env.action_space
+    self.discount=0.9
+    
+
+  def train_init(self, rng_key):
+    pass
+
+  def episode_init(self, initial_observation):
+    pass
+
+  def act(self, rng):
+    rand_action = rng.integers(0, self.action_space.n)
+    return rand_action, self.discount
+  
+  def observe(self, observation):
+    pass
+  
+  def remember(self, action, timestep):
+    pass
+
+  def learn_one_ep(self, episode):
+    pass
+
+  def get_stats(self):
+    pass
+
+  def learn_batch_transitions(self, transitions):
+    pass
